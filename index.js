0f0b17e76ae52840e15b52435c846b7e0651fd1c
Cole Waggoner <waggcj02@gmail.com>
2023-01-20 16:16:27 -0500

    Added questions to prompt in an array

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generate = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generate = require("./utils/generateMarkdown");
 
-console.log('Welcome. Please answer questions to generate a README file.')
+console.log("Welcome. Please answer questions to generate a README file.");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is your password?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please enter nstallation instructions:",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "Enter important usage information:",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Please enter contribution guidelines:",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message: "Please enter testing instructions:",
+    name: "tests",
+  },
+  {
+    type: "list",
+    choices: ["MIT", "Apache", "BSD", "Unlicense"],
+    message: "Re-enter password to confirm:",
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "Please enter your GitHub username:",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "Please enter your email:",
+    name: "email",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
